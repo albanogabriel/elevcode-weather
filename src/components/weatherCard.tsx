b4d4ecac44d87fa9867ae6f154dc2b73797b88b7
Gabriel Albano <albanogabriel33@gmail.com>
2024-06-21 11:39:16 -0300

    static v2

--- src/components/weatherCard.tsx
+++ src/components/weatherCard.tsx
@@ -4,8 +4,21 @@ import cloudWeather from '../assets/cloud.png'
 
 import humidity from '../assets/humidity.png'
 import wind from '../assets/wind.png'
+import { useEffect } from "react";
 
 export function WeatherCard(){
+
+  const apiKey = 'B1xAqIzjPNl63bKv'
+
+  async function getWeather(){
+    const response = await fetch(`https://my.meteoblue.com/packages/basic-1h_basic-day?apikey=${apiKey}&lat=47.5584&lon=7.57327&asl=279&format=json`)
+    console.log(response)
+  }
+
+  useEffect(() => {
+    getWeather()
+  }, [])
+
   return(
     <div style={{ display:'flex', justifyContent: 'space-between', flexDirection:'column', gap: '16px', alignItems: 'center', width: '400px', height: '600px', padding: '40px', border: '1px solid rgba(255, 255, 255, 0.5)', background: 'rgba(255, 255, 255, 0.4)', borderRadius: '16px'}}>
       <WeatherInput />
--- src/components/weatherInput.tsx
+++ src/components/weatherInput.tsx
@@ -5,7 +5,7 @@ export function WeatherInput(){
     <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between', padding: '8px', border: '1px solid rgba(255, 255, 255, 0.5)', borderRadius: '8px' }}>
       <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
         <MapPin color={'white'} size={18}/>
-        <h2 style={{ fontSize: '1.25rem' }}>Spain</h2>
+        <input style={{ fontSize: '1.25rem', color: 'white', background: 'transparent', border: 'none' }}/>
       </div>
       <button style={{ background: 'transparent' , border: 'none' }}>
         <MagnifyingGlass color="white" size={18} />
